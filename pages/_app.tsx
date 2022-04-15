8ca923e013263065124ee8874302572b52c8d08d
Lucas-Ayabe <lucas.ayabe@gmail.com>
2022-04-15 02:28:43 -0300

    refactor: _app.tsx component renamed from MyApp to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../modules/shared/styles/app.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default App;
